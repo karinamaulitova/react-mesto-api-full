08161f2ca7d5910a17f0da1798969729651eb0bf
Karina Maulitova <karinamaulitova@gmail.com>
2021-06-16 17:21:27 +0300

    fix: secret token

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -2,8 +2,6 @@
 const { NODE_ENV, JWT_SECRET } = process.env;
 const jwtSecret = NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'
 
-console.log(jwtSecret);
-
 module.exports = {
     jwtSecret
 }
